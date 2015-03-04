d12da1a0655c44374dd81314d1538e4aa5628c54
embruna <embruna@ufl.edu>
2015-03-04 00:00:52 -0500

    it works! it works!

--- TestCode.R
+++ TestCode.R
@@ -1,6 +1,8 @@
 #Trying to modify this to do multi-country mapping
 #http://stackoverflow.com/questions/5126745/gadm-maps-cross-country-comparision-graphics
-#WORKS
+library(sp)
+rm(list=ls())
+
 setwd("/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping")
 source("loadChangePrefix.R")
 source("changeGADMPrefix.R")
@@ -8,22 +10,20 @@ source("loadGADM.R")
 source("getCountries.R")
 
 
-test<-(c("BRL","VEN","CHL"))
-spdf <- getCountries("test")
-plot(spdf) # should plot all three
 
+baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/"
+arg <- getCountries('ARG', baseurl)
+plot(arg)
 
-#Note!!!
-#This snippet below will plot Brazil IF I use:  
-#load("/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/", fileName, "_adm0.RData", sep     = "")
-#in loadGADM
+baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/"
+bra <- getCountries('BRA', baseurl)
+plot(bra)
 
-test<-"BRL"
-spdf <- getCountries("test")
-plot(spdf) # should plot just Brazil
+baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/"
+ven <- getCountries('VEN', baseurl)
+plot(ven)
 
-
-load("/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/BRA_adm0.RData")
-load("/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/VEN_adm0.RData")
-load("/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/CHL_adm0.RData")
-
+baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/"
+ALL<-(c('BRA','VEN','ARG'))
+together <- getCountries(ALL, baseurl)
+plot(together)
