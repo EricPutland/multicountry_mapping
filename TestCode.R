e72260a0f4684482cdf0f14386c0d10f1439a441
Emilio Bruna <embruna@ufl.edu>
2015-03-05 14:08:34 -0500

    Update TestCode.R

--- TestCode.R
+++ TestCode.R
@@ -3,27 +3,27 @@
 library(sp)
 rm(list=ls())
 
-setwd("/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping")
+setwd("/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAmScience/multicountry_mapping")
 source("loadChangePrefix.R")
 source("changeGADMPrefix.R")
 source("loadGADM.R")
 source("getCountries.R")
 
 
-
-baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/"
+#P{lace the files you downloaded from GADM in your WD
+baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAmScience/multicountry_mapping"
 arg <- getCountries('ARG', baseurl)
 plot(arg)
 
-baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/"
+baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAmScience/multicountry_mapping"
 bra <- getCountries('BRA', baseurl)
 plot(bra)
 
-baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/"
+baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAmScience/multicountry_mapping"
 ven <- getCountries('VEN', baseurl)
 plot(ven)
 
-baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAm Science (Bruna & Hahn)/multicountry_mapping/"
+baseurl <-"/Volumes/ifas/Emilio's Folder Current/RESEARCH/LatAmScience/multicountry_mapping"
 ALL<-(c('BRA','VEN','ARG'))
 together <- getCountries(ALL, baseurl)
 plot(together)
